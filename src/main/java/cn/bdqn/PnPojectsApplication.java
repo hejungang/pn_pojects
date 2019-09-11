255da55cf709eed80100fb033dc9c77cdedefee0
张镭 <zl_javadream@163.com>
2019-09-11 16:43:22 +0800

    sss

---
+++ src/main/java/cn/bdqn/PnPojectsApplication.java
@@ -0,0 +1,13 @@
+package cn.bdqn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnPojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnPojectsApplication.class, args);
+	}
+
+}
